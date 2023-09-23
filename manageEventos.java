9708ebe599ee752a7a2725dc790e79e74054d456
Ian Cumes <ianrodrigocumes@gmail.com>
2023-09-22 21:53:31 -0600

    Arreglo alv

--- manageEventos.java
+++ manageEventos.java
@@ -127,7 +127,7 @@ public class manageEventos {
                 String nombre = partes[1];
                 Pais pais = new Pais(id, nombre);
                 listaPaises.add(pais);
-                escribirPaisesEnCSV(listaPaises);
+                
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -150,7 +150,7 @@ public class manageEventos {
                 int capacidad = Integer.parseInt(partes[3]);
                 Recinto recinto = new Recinto(idPais, ubicacion, idRecinto, capacidad);
                 listaRecintos.add(recinto);
-                escribirRecintosEnCSV(listaRecintos);
+                
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -176,7 +176,7 @@ public class manageEventos {
                 int asistentes = Integer.parseInt(partes[6]);
                 Evento evento = new Evento(idEvento, idPais, artista, hora, fecha, duracion, asistentes);
                 listaEventos.add(evento);
-                escribirEventosEnCSV(listaEventos);
+                
             }
         } catch (IOException e) {
             e.printStackTrace();
