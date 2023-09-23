6ffcacda91969244f325ba1c37c5e5bbd71de729
Javiervalladares1 <javivalladares10@icloud.com>
2023-09-22 21:05:37 -0600

    leer csv

--- manageEventos.java
+++ manageEventos.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class manageEventos {
     private List<Pais> paises;
@@ -13,17 +13,33 @@ public class manageEventos {
         paises = new ArrayList<>();
         recintos = new ArrayList<>();
         eventos = new ArrayList<>();
-        paises = cargarPaisesDesdeCSV("Pais.csv");
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar el nombre del archivo CSV para Paises
+        System.out.print("Ingrese el nombre del archivo CSV de Paises: ");
+        String archivoPaises = scanner.nextLine();
+        paises = cargarPaisesDesdeCSV(archivoPaises);
+
+        // Solicitar el nombre del archivo CSV para Recintos
+        System.out.print("Ingrese el nombre del archivo CSV de Recintos: ");
+        String archivoRecintos = scanner.nextLine();
+        recintos = cargarRecintosDesdeCSV(archivoRecintos);
+
+        // Solicitar el nombre del archivo CSV para Eventos
+        System.out.print("Ingrese el nombre del archivo CSV de Eventos: ");
+        String archivoEventos = scanner.nextLine();
+        eventos = cargarEventosDesdeCSV(archivoEventos);
     }
 
     public void agregarPais() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el nombre del país:");
         String nombrePais = scanner.nextLine();
+
         int nuevoIdPais = obtenerNuevoIdPais();
         Pais nuevoPais = new Pais(nuevoIdPais, nombrePais);
         paises.add(nuevoPais);
-        escribirPaisEnCSV(nuevoPais);
+        escribirPaisesEnCSV(paises);
         System.out.println("País agregado con éxito.");
     }
 
@@ -40,10 +56,11 @@ public class manageEventos {
         String ubicacion = scanner.nextLine();
         System.out.print("Ingrese la capacidad del recinto: ");
         int capacidad = scanner.nextInt();
+
         int nuevoIdRecinto = obtenerNuevoIdRecinto();
         Recinto nuevoRecinto = new Recinto(idPais, ubicacion, nuevoIdRecinto, capacidad);
         recintos.add(nuevoRecinto);
-        escribirRecintoEnCSV(nuevoRecinto);
+        escribirRecintosEnCSV(recintos);
         System.out.println("Recinto agregado con éxito.");
     }
 
@@ -71,25 +88,15 @@ public class manageEventos {
         scanner.nextLine();
         System.out.print("Ingrese el nombre del artista: ");
         String artista = scanner.nextLine();
-
-        int hora;
-        do {
-            System.out.print("Ingrese el horario (de 6 a 22): ");
-            hora = scanner.nextInt();
-        } while (hora < 6 || hora > 22);
-        scanner.nextLine(); // Consumir la nueva línea
-
+        System.out.print("Ingrese el horario (de 6 a 22): ");
+        int hora = scanner.nextInt();
+        scanner.nextLine();
         System.out.print("Ingrese la fecha (en formato AAAA-MM-DD): ");
         String fechaStr = scanner.nextLine();
         LocalDate fecha = LocalDate.parse(fechaStr);
-
-        int duracion;
-        do {
-            System.out.print("Ingrese la duración (de 1 a 12 horas): ");
-            duracion = scanner.nextInt();
-        } while (duracion < 1 || duracion > 12);
+        System.out.print("Ingrese la duración (de 1 a 12 horas): ");
+        int duracion = scanner.nextInt();
         scanner.nextLine();
-
         System.out.print("Ingrese la cantidad de asistentes (de 10 a 150000): ");
         int asistentes = scanner.nextInt();
         scanner.nextLine();
@@ -97,13 +104,125 @@ public class manageEventos {
         int nuevoIdEvento = obtenerNuevoIdEvento();
         Evento nuevoEvento = new Evento(nuevoIdEvento, idPais, artista, hora, fecha, duracion, asistentes);
         eventos.add(nuevoEvento);
-        escribirEventoEnCSV(nuevoEvento);
+        escribirEventosEnCSV(eventos);
         System.out.println("Evento agregado con éxito.");
     }
 
     public void verTodosLosEventos() {
         for (Evento evento : eventos) {
             mostrarEvento(evento);
+        }
+    }
+
+    private List<Pais> cargarPaisesDesdeCSV(String archivoCSV) {
+        List<Pais> listaPaises = new ArrayList<>();
+        try (BufferedReader lector = new BufferedReader(new FileReader(archivoCSV))) {
+            // Leer y descartar la primera línea (encabezado)
+            lector.readLine();
+            
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                String[] partes = linea.split(",");
+                int id = Integer.parseInt(partes[0]);
+                String nombre = partes[1];
+                Pais pais = new Pais(id, nombre);
+                listaPaises.add(pais);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaPaises;
+    }
+
+    private List<Recinto> cargarRecintosDesdeCSV(String archivoCSV) {
+        List<Recinto> listaRecintos = new ArrayList<>();
+        try (BufferedReader lector = new BufferedReader(new FileReader(archivoCSV))) {
+            // Leer y descartar la primera línea (encabezado)
+            lector.readLine();
+            
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                String[] partes = linea.split(",");
+                int idPais = Integer.parseInt(partes[0]);
+                String ubicacion = partes[1];
+                int idRecinto = Integer.parseInt(partes[2]);
+                int capacidad = Integer.parseInt(partes[3]);
+                Recinto recinto = new Recinto(idPais, ubicacion, idRecinto, capacidad);
+                listaRecintos.add(recinto);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaRecintos;
+    }
+    
+    private List<Evento> cargarEventosDesdeCSV(String archivoCSV) {
+        List<Evento> listaEventos = new ArrayList<>();
+        try (BufferedReader lector = new BufferedReader(new FileReader(archivoCSV))) {
+            // Leer y descartar la primera línea (encabezado)
+            lector.readLine();
+            
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                String[] partes = linea.split(",");
+                int idEvento = Integer.parseInt(partes[0]);
+                int idPais = Integer.parseInt(partes[1]);
+                String artista = partes[2];
+                int hora = Integer.parseInt(partes[3]);
+                LocalDate fecha = LocalDate.parse(partes[4]);
+                int duracion = Integer.parseInt(partes[5]);
+                int asistentes = Integer.parseInt(partes[6]);
+                Evento evento = new Evento(idEvento, idPais, artista, hora, fecha, duracion, asistentes);
+                listaEventos.add(evento);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listaEventos;
+    }
+    
+
+    private void escribirPaisesEnCSV(List<Pais> paises) {
+        try (BufferedWriter escritor = new BufferedWriter(new FileWriter("Pais.csv", false))) {
+            escritor.write("ID,Nombre");
+            escritor.newLine();
+            for (Pais pais : paises) {
+                String linea = pais.getId() + "," + pais.getNombre();
+                escritor.write(linea);
+                escritor.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void escribirRecintosEnCSV(List<Recinto> recintos) {
+        try (BufferedWriter escritor = new BufferedWriter(new FileWriter("Recinto.csv", false))) {
+            escritor.write("ID_Pais,Ubicacion,ID,Capacidad");
+            escritor.newLine();
+            for (Recinto recinto : recintos) {
+                String linea = recinto.getIdPais() + "," + recinto.getUbicacion() + "," + recinto.getId() + "," + recinto.getCapacidad();
+                escritor.write(linea);
+                escritor.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void escribirEventosEnCSV(List<Evento> eventos) {
+        try (BufferedWriter escritor = new BufferedWriter(new FileWriter("Eventos.csv", false))) {
+            escritor.write("ID,ID_Pais,Artista,Hora,Fecha,Duracion,Asistentes");
+            escritor.newLine();
+            for (Evento evento : eventos) {
+                String linea = evento.getId() + "," + evento.getIdPais() + "," + evento.getArtista() + ","
+                        + evento.getHora() + "," + evento.getFecha() + "," + evento.getDuracion() + ","
+                        + evento.getAsistentes();
+                escritor.write(linea);
+                escritor.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -135,80 +254,6 @@ public class manageEventos {
             }
         }
         return ultimoIdEvento + 1;
-    }
-
-    private void escribirPaisEnCSV(Pais pais) {
-        File archivo = new File("Pais.csv");
-        boolean archivoExiste = archivo.exists();
-    
-        try (BufferedWriter escritor = new BufferedWriter(new FileWriter(archivo, true))) {
-            if (!archivoExiste) {
-                escritor.write("ID,Pais");
-                escritor.newLine();
-            }
-    
-            String linea = pais.getId() + "," + pais.getNombre();
-            escritor.write(linea);
-            escritor.newLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    private void escribirRecintoEnCSV(Recinto recinto) {
-        File archivo = new File("Recinto.csv");
-        boolean archivoExiste = archivo.exists();
-    
-        try (BufferedWriter escritor = new BufferedWriter(new FileWriter(archivo, true))) {
-            if (!archivoExiste) {
-                escritor.write("IDPaís,Ubicación,IDRecinto,Capacidad");
-                escritor.newLine();
-            }
-    
-            String linea = recinto.getIdPais() + "," + recinto.getUbicacion() + "," + recinto.getId() + "," + recinto.getCapacidad();
-            escritor.write(linea);
-            escritor.newLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    private void escribirEventoEnCSV(Evento evento) {
-        File archivo = new File("Eventos.csv");
-        boolean archivoExiste = archivo.exists();
-    
-        try (BufferedWriter escritor = new BufferedWriter(new FileWriter(archivo, true))) {
-            if (!archivoExiste) {
-                escritor.write("ID,IDPaís,Artista,Hora,Fecha,Duración,Asistentes");
-                escritor.newLine();
-            }
-    
-            String linea = evento.getId() + "," + evento.getIdPais() + "," + evento.getArtista() + ","
-                    + evento.getHora() + "," + evento.getFecha() + "," + evento.getDuracion() + ","
-                    + evento.getAsistentes();
-            escritor.write(linea);
-            escritor.newLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-
-    private List<Pais> cargarPaisesDesdeCSV(String archivoCSV) {
-        List<Pais> listaPaises = new ArrayList<>();
-        try (BufferedReader lector = new BufferedReader(new FileReader(archivoCSV))) {
-            String linea;
-            while ((linea = lector.readLine()) != null) {
-                String[] partes = linea.split(",");
-                int id = Integer.parseInt(partes[0]);
-                String nombre = partes[1];
-                Pais pais = new Pais(id, nombre);
-                listaPaises.add(pais);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return listaPaises;
     }
 
     private Recinto obtenerRecintoPorId(int id) {
